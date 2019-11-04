81e942a624180b137575b468f5c7d0fe5336a6b5
Nirawat P <fovexlazy@gmail.com>
2019-11-05 04:30:37 +0700

    [UPDATE]Nav

---
+++ src/Component/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Nav.css';
+
+const Navbar= (props) => {
+    <header className="Navbar">
+      <nav className="Navbar_Nav">
+      <div></div>
+      <div className="Navlogo"><a href="/">logo</a></div>
+      <div className="NavItem">
+        <ul>
+          <li><a href="/">HOME</a></li>
+          <li><a href="/">ABOUT</a></li>
+        </ul>
+      </div>
+      </nav>
+    </header>
+};
+
+export default Navbar;
