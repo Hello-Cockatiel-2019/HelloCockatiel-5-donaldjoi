537ed3df2660f79250eb865cb9788462aef451fa
nattanunkorn <nattanunkorn.cake@mail.kmutt.ac.th>
2019-11-05 03:34:49 +0700

    [UPDATE]Font

---
+++ src/Component/background.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+const FixedBackground = styled.div`
+  width:100%;
+  height: 100vh;
+  background-repeat:no-repeat;
+  background-position: absolute;
+  position:fixed;
+  background-size:.......;
+  background-image:url('/background/cover.jpggit');
+    @bg (max-width: 1440px){
+      background-image:url('/img/bg1.png');
+    }
+    @bg (max-width: 1440px) {
+      background-image:url('/img/bg2.png');
+    }
+    @bg (max-width: 1440px) {
+        background-image:url('/img/bg3.png');
+      }
+   
+`
+export default class background extends Component {
+  render () {
+    return (
+      <FixedBackground/>
+    )
+  }
+}
---
+++ src/Config/Font.js
@@ -0,0 +1,22 @@
+
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    @font{
+        font-family: "Itim";
+        font-style: normal;
+        font-weight: bold;
+        font-size: 22px;
+        line-height: 27px;
+
+    }
+    body{
+        font-family: "Itim";
+        font-style: normal;
+        font-weight: normal;
+        font-size: 30px;
+        line-height: 36px;
+    }
+`
+
+export default GlobalStyle
