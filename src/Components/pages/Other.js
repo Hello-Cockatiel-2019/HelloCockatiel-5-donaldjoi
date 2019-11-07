1c994d81db0117d1084b04ff9e49488f4a07e3d8
Nirawat P <fovexlazy@gmail.com>
2019-11-07 17:07:13 +0700

    [UPDATE]Semifinalllll

--- src/Components/pages/Other.js
+++ src/Components/pages/Other.js
@@ -21,7 +21,7 @@ class About extends React.Component {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-md-6">
-                        <img className="wanted" src="/images/Food.png" style={{position: 'absolute', zIndex: '1', width: '350px', height: '250px', marginTop: '225px',marginLeft: '600px'}}/>
+                        <img className="wanted" src="images/Food.png" style={{position: 'absolute', zIndex: '1', width: '350px', height: '250px', marginTop: '225px',marginLeft: '600px'}}/>
                             <Text style={{ position: 'absolute', zIndex: '0', marginTop: '550px', marginLeft: '100px' }}>
                                 <Textbox data="ห่านกินอะไร? หญ้าหรือผักชนิดต่างๆ เช่น รำข้าวหรือข้าวโพดป่น ถั่วเขียวหรือถั่วเหลืองป่น" />
                             </Text>
@@ -34,7 +34,7 @@ class About extends React.Component {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-6">
-                    <img className="wanted" src="/images/Digg.png" style={{position: 'absolute', zIndex: '1', width: '350px', height: '250px', marginTop: '225px',marginLeft: '575px'}}/>
+                    <img className="wanted" src="images/Digg.png" style={{position: 'absolute', zIndex: '1', width: '350px', height: '250px', marginTop: '225px',marginLeft: '575px'}}/>
                         <Text style={{ position: 'absolute', zIndex: '0', marginTop: '550px', marginLeft: '100px' }}>
                             <Textbox data="ประโยชน์ของการเลี้ยงห่าน 1.เลี้ยงง่าย เจริญเติบโตเร็ว 2.ช่วยทำให้พื้นที่สะอาด ห่านสามารถกินหญ้าหรือวัชพืชต่างๆ ได้เป็นอย่างดี จึงช่วยทำให้บริเวณที่เลี้ยงสะอาด 3.มูลห่านใช้เป็นปุ๋ยสำหรับใส่ต้นไม้และพืชผักได้ 4.ช่วยเฝ้าบ้านและป้องกันสัตว์ร้ายในบริเวณบ้าน เช่น แมลงป่อง ตะขาบ และ งู" />
                         </Text>
